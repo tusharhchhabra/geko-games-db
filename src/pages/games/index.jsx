8d4d8e388b80220de94151923782412c0e4e9216
Brett Roberts <brett.roberts@live.ca>
2023-10-28 21:00:31 -0600

    Add API calls to fetch top games and correspondinig cover art

--- src/pages/games/index.jsx
+++ src/pages/games/index.jsx
@@ -1,21 +1,38 @@
 import fetchGames from "@/helpers/fetchDataExample";
+import GamesList from "@/components/GameList";
+import extractId from "@/helpers/extractId";
+import adjustImageUrl from "@/helpers/adjustImageUrl";
 
-function GamesList({ games }) {
+const HomePage = ({ combinedData }) => {
   return (
     <div>
-      {games &&
-        games.length > 0 &&
-        games.map((game) => <p key={game.id}>{game.name}</p>)}
+      <GamesList combinedData={combinedData} />
     </div>
   );
 }
 
-export default GamesList;
-
-const query = "fields name; limit 10;";
-const endpoint = "games"
+export default HomePage;
 
 export async function getServerSideProps() {
-  const games = await fetchGames(query, endpoint);
-  return { props: { games } };
+
+  // API call to get the games
+  const query1 = "fields name, id; where total_rating_count >= 100; sort total_rating desc; limit 12;";
+  const games = await fetchGames(query1, "games");
+
+  // API call to get the covers
+  const query2 = `fields game, url; where game = ${extractId(games)};`;
+  const covers = await fetchGames(query2, "covers");
+
+  // Combine the data
+  const combinedData = games.map(game => {
+    const cover = covers.find(cover => cover.game === game.id);
+    const coverUrl = cover ? adjustImageUrl(cover.url) : null;
+    return {
+      ...game,
+      coverUrl
+    };
+
+  });
+  
+  return { props: { combinedData } };
 }
