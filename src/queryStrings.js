6f1bd51573c74ac3752d6756184605f908e5d039
Brett Roberts <brett.roberts@live.ca>
2023-10-29 01:35:47 -0600

    Add query to find new games

--- src/queryStrings.js
+++ src/queryStrings.js
@@ -1,10 +1,13 @@
 import extractId from "./helpers/extractId";
 import adjustImageUrl from "./helpers/adjustImageUrl";
+import timestamp30DaysAgo from "./helpers/findTime";
 
 const queries = {
     top10Games: "fields name, id; where total_rating_count >= 100; sort total_rating desc; limit 10;",
 
     actionGames: "fields id, name; where total_rating_count >= 100 & themes = 1; sort total_rating desc; limit 10;",
+    
+    newGames: `fields id, name, first_release_date; where total_rating_count >= 5 & first_release_date > ${timestamp30DaysAgo}; sort total_rating desc; limit 10;`,
 
     coverArt: function (games) {
       return `fields game, url; where game = ${extractId(games)};` 
@@ -19,8 +22,10 @@ const queries = {
               coverUrl
           };
       });
-  }
-  
+  },
+
+
+
 }
 
 export default queries;
