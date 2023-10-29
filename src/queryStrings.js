f8edeece7550a2c0045f94c88d8650ec6ec239a6
Brett Roberts <brett.roberts@live.ca>
2023-10-29 11:44:53 -0600

    refactor getTimeStamp30DaysAgo into a function

--- src/queryStrings.js
+++ src/queryStrings.js
@@ -1,6 +1,6 @@
 import extractId from "./helpers/extractId";
 import adjustImageUrl from "./helpers/adjustImageUrl";
-import timestamp30DaysAgo from "./helpers/findTime";
+import { getTimestamp30DaysAgo } from "./helpers/findTime";
 
 const queries = {
   top10Games:
@@ -10,7 +10,7 @@ const queries = {
     "fields id, name; where total_rating_count >= 100 & themes = 1; sort total_rating desc; limit 10;",
     
   newGames: 
-  `fields id, name, first_release_date; where total_rating_count >= 5 & first_release_date > ${timestamp30DaysAgo}; sort total_rating desc; limit 10;`,
+  `fields id, name, first_release_date; where total_rating_count >= 5 & first_release_date > ${getTimestamp30DaysAgo()}; sort total_rating desc; limit 10;`,
 
   coverArt: function (games) {
     return `fields game, url; where game = ${extractId(games)};`;
