579721895876ac06d6020da295327c9f1578bec8
Brett Roberts <brett.roberts@live.ca>
2023-10-29 01:00:31 -0600

    Refactor to pass only a single prop into Gamelist component. Make each set of games into an object with a title key and games key.

--- src/pages/games/index.jsx
+++ src/pages/games/index.jsx
@@ -2,10 +2,10 @@ import fetchGames from "@/helpers/fetchDataExample";
 import GamesList from "@/components/GameList";
 import queries from "@/queryStrings";
 
-const HomePage = ({ top10GamesWithCovers, actionGamesWithCovers }) => {
+const HomePage = ({ setOfGames }) => {
   return (
     <div>
-      <GamesList top10Games={top10GamesWithCovers} actionGames={actionGamesWithCovers} />
+      <GamesList setOfGames={setOfGames} />
     </div>
   );
 }
@@ -18,11 +18,15 @@ export async function getServerSideProps() {
   const top10Games = await fetchGames(queries.top10Games, "games");
   const covers = await fetchGames(queries.coverArt(top10Games), "covers");
   const top10GamesWithCovers = queries.gamesWithCoverArt(top10Games, covers)
-  
+  top10GamesWithCovers
+  const top10GamesObject = {games: top10GamesWithCovers, title: "Top 10"};
   // Action Games
   const actionGames = await fetchGames(queries.actionGames, "games");
   const actionCovers = await fetchGames(queries.coverArt(actionGames), "covers");
   const actionGamesWithCovers = queries.gamesWithCoverArt(actionGames, actionCovers);
+  const actionGamesObject = {games: actionGamesWithCovers, title: "Action Games"};
+
+  const setOfGames = [top10GamesObject, actionGamesObject]
  
-  return { props: { top10GamesWithCovers, actionGamesWithCovers } };
+  return { props: { setOfGames } };
 }
