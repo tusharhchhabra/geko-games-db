ffd127d564ebb54ef0dd648a38c97bc0d9b429d9
Brett Roberts <brett.roberts@live.ca>
2023-10-29 22:55:29 -0600

    Refactor index.js to only fetch new games when a user scrolls to the bottom of a page and make game fetching dynamic based on themes

--- src/pages/games/index.jsx
+++ src/pages/games/index.jsx
@@ -1,13 +1,76 @@
+import React, { useState, useEffect, useCallback } from "react";
 import fetchData from "@/helpers/fetchData";
 import GamesList from "@/components/GameList";
-import queries from "@/queryStrings";
 import SearchBar from "../components/SearchBar";
+import queries from "@/queryStrings";
+import extractIdAsAnArray from "@/helpers/extractIdsAsArray";
+import extractThemeAsAnArray from "@/helpers/extractThemeAsArray";
 
-const HomePage = ({ setOfGames }) => {
+const HomePage = ({ initialGameSets, initialThemes }) => {
+  const [gameSets, setGameSets] = useState(initialGameSets);
+  const [loading, setLoading] = useState(false);
+  const [allDataLoaded, setAllDataLoaded] = useState(false);
+  const [gameThemesId, setGameThemesId] = useState(() =>
+    extractIdAsAnArray(initialThemes)
+  );
+  const [gameThemes, setGameThemes] = useState(() =>
+    extractThemeAsAnArray(initialThemes)
+  );
+
+  const fetchMoreGames = useCallback(async () => {
+    if (!gameThemesId.length && !gameThemes.length) {
+      setAllDataLoaded(true);
+      return;
+    }
+
+    setLoading(true);
+
+    const [nextThemeId, ...remainingThemeId] = gameThemesId;
+    const [nextTheme, ...remainingThemeNames] = gameThemes;
+    try {
+      const response = await fetch(
+        `/api/games?nextThemeId=${encodeURIComponent(
+          nextThemeId
+        )}&nextTheme=${encodeURIComponent(nextTheme)}`
+      );
+      const newGameData = await response.json();
+
+      setGameSets((prevGames) => [...prevGames, newGameData]);
+      setGameThemesId(remainingThemeId);
+      setGameThemes(remainingThemeNames);
+    } catch (error) {
+      console.error("Error fetching additional games: ", error);
+    } finally {
+      setLoading(false);
+    }
+  }, [gameThemesId, gameThemes]);
+
+  const handleScroll = useCallback(() => {
+    if (loading || allDataLoaded) return;
+
+    const scrollTop =
+      document.documentElement.scrollTop || document.body.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight;
+    const clientHeight = document.documentElement.clientHeight;
+
+    const scrolledToBottom =
+      Math.ceil(scrollTop + clientHeight) >= scrollHeight;
+
+    if (scrolledToBottom) {
+      fetchMoreGames();
+    }
+  }, [loading, allDataLoaded, fetchMoreGames]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
+
   return (
     <div className="p-10">
       <SearchBar />
-      <GamesList setOfGames={setOfGames} />
+      <GamesList setOfGames={gameSets} />
+      {loading && <p>Loading more games...</p>}
     </div>
   );
 };
@@ -15,44 +78,45 @@ const HomePage = ({ setOfGames }) => {
 export default HomePage;
 
 export async function getServerSideProps() {
-  // Top 10 games
-  const top10Games = await fetchData(queries.top10Games, "games");
-  const covers = await fetchData(queries.coverArt(top10Games), "covers");
-  const top10GamesWithCovers = queries.gamesWithCoverArt(
-    top10Games,
-    covers,
-    "t_cover_big"
-  );
-  top10GamesWithCovers;
-  const top10GamesObject = { games: top10GamesWithCovers, title: "Top 10" };
+  try {
+    const themes = await fetchData(queries.themes, "themes");
 
-  // Action Games
-  const actionGames = await fetchData(queries.actionGames, "games");
-  const actionCovers = await fetchData(queries.coverArt(actionGames), "covers");
-  const actionGamesWithCovers = queries.gamesWithCoverArt(
-    actionGames,
-    actionCovers,
-    "t_cover_big"
-  );
-  const actionGamesObject = {
-    games: actionGamesWithCovers,
-    title: "Action Games",
-  };
+    const top10Games = await fetchData(queries.top10Games, "games");
+    const covers = await fetchData(queries.coverArt(top10Games), "covers");
+    const top10GamesWithCovers = queries.gamesWithCoverArt(
+      top10Games,
+      covers,
+      "t_cover_big"
+    );
 
-  // New and Noteworthy Games
-  const newGames = await fetchData(queries.newGames, "games");
-  const newCovers = await fetchData(queries.coverArt(newGames), "covers");
-  const newGamesWithCovers = queries.gamesWithCoverArt(
-    newGames,
-    newCovers,
-    "t_cover_big"
-  );
-  const newGamesObject = {
-    games: newGamesWithCovers,
-    title: "New and Noteworthy"
+    const top10GamesObject = { games: top10GamesWithCovers, title: "Top 10" };
+
+    const newGames = await fetchData(queries.newGames, "games");
+    const newCovers = await fetchData(queries.coverArt(newGames), "covers");
+    const newGamesWithCovers = queries.gamesWithCoverArt(
+      newGames,
+      newCovers,
+      "t_cover_big"
+    );
+
+    const newGamesObject = {
+      games: newGamesWithCovers,
+      title: "New and Noteworthy",
     };
 
-  const setOfGames = [top10GamesObject, actionGamesObject, newGamesObject]
- 
-  return { props: { setOfGames } };
+    return {
+      props: {
+        initialGameSets: [top10GamesObject, newGamesObject],
+        initialThemes: themes,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching initial data: ", error);
+    return {
+      props: {
+        initialGameSets: [],
+        initialThemes: [],
+      },
+    };
+  }
 }
