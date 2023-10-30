ca2261b4d0cf42a06e384e0d25327a798da62746
Brett Roberts <brett.roberts@live.ca>
2023-10-29 22:52:12 -0600

    Add themes and gamesByTheme queries

--- src/queryStrings.js
+++ src/queryStrings.js
@@ -6,21 +6,19 @@ const queries = {
   top10Games:
     "fields name, id; where total_rating_count >= 100; sort total_rating desc; limit 10;",
 
-  actionGames:
-    "fields id, name; where total_rating_count >= 100 & themes = 1; sort total_rating desc; limit 10;",
-    
-  newGames: 
-  `fields id, name, first_release_date; where total_rating_count >= 5 & first_release_date > ${getTimestamp30DaysAgo()}; sort total_rating desc; limit 10;`,
+  newGames: `fields id, name, first_release_date; where total_rating_count >= 5 & first_release_date > ${getTimestamp30DaysAgo()}; sort total_rating desc; limit 10;`,
 
+  themes: "fields id, name;",
+
+  gamesByTheme: function gamesByTheme(themeId) {
+    return `fields name, id; where total_rating_count >= 10 & themes = ${themeId}; sort total_rating desc; limit 10;`;
+  },
+  
   coverArt: function (games) {
     return `fields game, url; where game = ${extractId(games)};`;
   },
 
-  gamesWithCoverArt: function top10GamesWithCoverArt(
-    games,
-    covers,
-    coverFormat
-  ) {
+  gamesWithCoverArt: function gamesWithCoverArt(games, covers, coverFormat) {
     return games.map((game) => {
       const cover = covers.find((cover) => cover.game === game.id);
       const coverUrl = cover ? adjustImageUrl(cover.url, coverFormat) : null;
