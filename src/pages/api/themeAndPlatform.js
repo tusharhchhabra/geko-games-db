2be5e1d29063e13ad901ff736733932f8aa67daf
Brett Roberts <brett.roberts@live.ca>
2023-10-31 02:26:16 -0600

    create themeAndPlatform api route

---
+++ src/pages/api/themeAndPlatform.js
@@ -0,0 +1,31 @@
+import fetchData from "@/helpers/fetchData";
+import queries from "@/helpers/queryStrings";
+
+export default async function search(req, res) {
+  if (req.method !== "GET") {
+    res.statusCode = 405;
+    res.end("Method Not Allowed");
+    return;
+  }
+
+  const { nextThemeId, nextTheme, platformId } = req.query;
+  console.log("req.query", req.query);
+  const gamesQuery = queries.gamesByThemeAndPlatform(nextThemeId, platformId);
+  const endpoint = "games";
+
+  const games = await fetchData(gamesQuery, endpoint);
+  const covers = await fetchData(queries.coverArtForGames(games), "covers");
+
+  const gamesWithCovers = queries.gamesWithCoverArt(
+    games,
+    covers,
+    "t_cover_big"
+  );
+
+  const gamesObject = {
+    games: gamesWithCovers,
+    title: nextTheme,
+  };
+
+  res.send(gamesObject);
+}
