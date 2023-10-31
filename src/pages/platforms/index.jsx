f3064ef95ab83a53e121273a79ed7b325a5dc1dd
Brett Roberts <brett.roberts@live.ca>
2023-10-30 20:46:45 -0600

    Add logic to fetch games by platforms on scroll

--- src/pages/platforms/index.jsx
+++ src/pages/platforms/index.jsx
@@ -1,40 +1,88 @@
 import GamesList from "@/components/GameList";
 import fetchData from "@/helpers/fetchData";
 import queries from "@/queryStrings";
+import SearchBar from "../components/SearchBar";
+import { useState, useEffect, useCallback } from "react";
 import extractId from "@/helpers/extractId";
+import extractIdAsAnArray from "@/helpers/extractIdsAsArray";
+import extractNameAsAnArray from "@/helpers/extractNameAsArray";
 
-const Platform = ({ initialGames, initialPlatforms }) => {
-  const [platform, setPlatform] = useState(null);
-  const [games, setGames] = useState(initialGames);
+const PlatformsGameList = ({ initialGames, initialPlatforms }) => {
+  const [gameSets, setGameSets] = useState(initialGames);
+  const [loading, setLoading] = useState(false);
+  const [allDataLoaded, setAllDataLoaded] = useState(false);
+  const [platformId, setPlatformId] = useState(() =>
+    extractIdAsAnArray(initialPlatforms));
+  const [gamePlatforms, setGamePlatform] = useState(() =>
+    extractNameAsAnArray(initialPlatforms)
+  );
+  
 
   const platformsDropdown = initialPlatforms.map((platform) => {
     return platform.name;
   });
 
-
-  const fetchNextSection = async (platform) => {
-    if (platform) {
-      const response = await fetch(
-        `/api/platforms?platformId=${encodeURIComponent(platform.id)}`
-      );
-      const results = await response.json();
-      setGames(results);
-    } else {
-      setGames([]);
+  const fetchMoreGames = useCallback(async () => {
+    if (!platformId.length && !platforms.length) {
+      setAllDataLoaded(true);
+      return;
     }
-  };
 
-  useEffect(() => {}, [platform]);
+    setLoading(true);
+
+    const [nextPlatformId, ...remainingPlatformId] = platformId;
+    const [nextPlatform, ...remainingPlatformNames] = gamePlatforms;
+    try {
+      const response = await fetch(
+        `/api/platforms?nextPlatformId=${encodeURIComponent(
+          nextPlatformId
+        )}&nextPlatform=${encodeURIComponent(nextPlatform)}`
+      );
+      const newGameData = await response.json();
+
+      setGameSets((prevGames) => [...prevGames, newGameData]);
+      setPlatformId(remainingPlatformId);
+      setGamePlatform(remainingPlatformNames);
+    } catch (error) {
+      console.error("Error fetching additional games: ", error);
+    } finally {
+      setLoading(false);
+    }
+  }, [gameSets, platformId]);
+
+
+  const handleScroll = useCallback(() => {
+    if (loading || allDataLoaded) return;
+
+    const scrollTop =
+      document.documentElement.scrollTop || document.body.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight;
+    const clientHeight = document.documentElement.clientHeight;
+
+    const scrolledToBottom =
+      Math.ceil(scrollTop + clientHeight) >= scrollHeight;
+
+    if (scrolledToBottom) {
+      fetchMoreGames();
+    }
+  }, [loading, allDataLoaded, fetchMoreGames]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
 
   return (
-    <div>
-      <h1>Hello</h1>
-      <GamesList setOfGames={setOfGames} />
+    <div className="p-10">
+      <SearchBar />
+      <GamesList setOfGames={gameSets} />
+      {loading && <p className="text-3xl font-bold text-gray-700 mt-4">Loading more games...</p>}
+
     </div>
   );
 };
 
-export default Platform;
+export default PlatformsGameList;
 
 export async function getServerSideProps() {
 
@@ -43,25 +91,50 @@ export async function getServerSideProps() {
 
   // Fetch initial games (Move rest to call on scroll)
   // (PS5 &  Xbox Series X/S)
-  const gamesByPlatform = await fetchData(
-    queries.gamesByPlatforms((167, 169)),
+  const seriesXGames = await fetchData(
+    queries.gamesByPlatform(169),
     "games"
   );
-  const gamesByPlatformCovers = await fetchData(
-    queries.coverArt(gamesByPlatform),
+  console.log(seriesXGames);
+
+
+
+  const seriesXGamesCovers = await fetchData(
+    queries.coverArt(seriesXGames),
     "covers"
   );
-  const gamesByPlatformWithCovers = queries.gamesWithCoverArt(
-    gamesByPlatform,
-    gamesByPlatformCovers,
+  const seriesXGamesWithCovers = queries.gamesWithCoverArt(
+    seriesXGames,
+    seriesXGamesCovers,
     "t_cover_big"
   );
-  const gamesByPlatformObject = {
-    games: gamesByPlatformWithCovers,
-    title: "All Platforms",
+  const seriesXGamesObject = {
+    games: seriesXGamesWithCovers,
+    title: "Xbox Series X/S",
+  };
+  console.log("obj", seriesXGamesObject);
+
+  const ps5Games = await fetchData(
+    queries.gamesByPlatform(167),
+    "games"
+  );
+  const ps5GamesCovers = await fetchData(
+    queries.coverArt(ps5Games),
+    "covers"
+  );
+  const ps5GamesWithCovers = queries.gamesWithCoverArt(
+    ps5Games,
+    ps5GamesCovers,
+    "t_cover_big"
+  );
+  const ps5GamesObject = {
+    games: ps5GamesWithCovers,
+    title: "PS5",
   };
 
-  const setOfGames = [gamesByPlatformObject];
+  const setOfGames = [seriesXGamesObject, ps5GamesObject];
 
   return { props: { initialGames: setOfGames, initialPlatforms: platforms } };
 }
+
+//fields name, id, total_rating; where platforms = [167] & rating > 70 & rating_count > 0; sort total_rating desc; limit 10;
