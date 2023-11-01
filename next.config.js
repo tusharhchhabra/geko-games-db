eb8c9a5f156461ad6f9c4eff0eb9312a7d608caa
Tushar Chhabra <tusharhchhabra@gmail.com>
2023-10-31 22:56:15 -0700

    Add redirect route to Next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: "/games",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
