a8323604fb94207d96a195ff1bafa153a1dfdd68
Tushar Chhabra <tusharhchhabra@gmail.com>
2023-11-02 12:50:14 -0700

    Add back logic

--- src/middleware.js
+++ src/middleware.js
@@ -2,34 +2,34 @@
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
-  // const token = req.cookies.auth_token;
-  // if (!token) {
-  //   console.log("No auth token found");
-  //   return new NextResponse(
-  //     JSON.stringify({ error: { message: "authentication required" } }),
-  //     { status: 401 }
-  //   );
-  // }
+  const token = req.cookies.auth_token;
+  if (!token) {
+    console.log("No auth token found");
+    return new NextResponse(
+      JSON.stringify({ error: { message: "authentication required" } }),
+      { status: 401 }
+    );
+  }
 
-  // console.log("Auth token found");
+  console.log("Auth token found");
 
-  // try {
-  // if (jwt.verify(token, process.env.JWT_SECRET)) {
-  return NextResponse.next();
-  // } else {
-  console.log("Invalid token", error);
-  return new NextResponse(
-    JSON.stringify({ error: { message: "authentication required" } }),
-    { status: 401 }
-  );
-  // }
-  // } catch (error) {
-  //   // console.log("Not authenticated", error);
-  //   return new NextResponse(
-  //     JSON.stringify({ error: { message: "authentication required" } }),
-  //     { status: 401 }
-  //   );
-  // }
+  try {
+    if (jwt.verify(token, process.env.JWT_SECRET)) {
+      return NextResponse.next();
+    } else {
+      console.log("Invalid token", error);
+      return new NextResponse(
+        JSON.stringify({ error: { message: "authentication required" } }),
+        { status: 401 }
+      );
+    }
+  } catch (error) {
+    // console.log("Not authenticated", error);
+    return new NextResponse(
+      JSON.stringify({ error: { message: "authentication required" } }),
+      { status: 401 }
+    );
+  }
 }
 
 export const config = {
