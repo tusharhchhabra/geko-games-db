e90d063f4f9eef57b2078a1eb226b47807be15ac
Tushar Chhabra <tusharhchhabra@gmail.com>
2023-11-02 15:28:06 -0700

    Clean up

--- src/middleware.js
+++ src/middleware.js
@@ -4,20 +4,16 @@ import { NextResponse } from "next/server";
 export async function middleware(req) {
   const token = req.cookies.auth_token;
   if (!token) {
-    console.log("No auth token found");
     return new NextResponse(
       JSON.stringify({ error: { message: "Authentication required" } }),
       { status: 401 }
     );
   }
 
-  console.log("Auth token found");
-
   try {
     await jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET));
     return NextResponse.next();
   } catch (error) {
-    console.log("Invalid token or verification failed", error);
     return new NextResponse(
       JSON.stringify({ error: { message: "Authentication required" } }),
       { status: 401 }
