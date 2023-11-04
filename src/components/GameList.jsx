5b5cd3760c77f80f0cb362bc0bc0a79088d3ca96
Brett Roberts <brett.roberts@live.ca>
2023-11-04 01:20:08 -0600

    Refactor slider

--- src/components/GameList.jsx
+++ src/components/GameList.jsx
@@ -1,55 +1,59 @@
-import React, { useRef } from 'react';
+import React, { useRef } from "react";
 import GamesListItem from "@/components/GameListItem";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 const GamesList = ({ setOfGames }) => {
-
   const sliderRefs = useRef(new Array(setOfGames.length).fill(null));
 
-const slide = (index, direction) => {
+  const slide = (index, direction) => {
     const slider = sliderRefs.current[index];
     if (slider) {
-      const scrollAmount = direction === 'left' ? -500 : 500;
+      const scrollAmount = direction === "left" ? -400 : 400;
       let newScrollPosition = slider.scrollLeft + scrollAmount;
 
-      if (direction === 'right' && newScrollPosition + slider.clientWidth > slider.scrollWidth) {
+      if (
+        direction === "right" &&
+        newScrollPosition + slider.clientWidth > slider.scrollWidth
+      ) {
         newScrollPosition = 0;
       }
 
-      if (direction === 'left' && newScrollPosition < 0) {
-        newScrollPosition = 1500;
+      if (direction === "left" && newScrollPosition < 0) {
+        newScrollPosition = 1200;
       }
 
       slider.scrollTo({
         left: newScrollPosition,
-        behavior: 'smooth'
+        behavior: "smooth",
       });
     }
-};
-
-  
-  
+  };
 
   return (
     <>
       {setOfGames.map((gameSet, index) => (
         <div key={index}>
-          <h2 className="text-white font-bold p-4 ml-10 text-3xl">
+          <h2 className="text-violet-500 font-bold p-4 ml-10 text-4xl [text-shadow:2px_2px_3px_var(--tw-shadow-color)] shadow-black ">
             {gameSet.title}
           </h2>
+
           <div className="relative flex items-center group">
             <MdChevronLeft
-              onClick={() => slide(index, 'left')}
-              className="bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 left-0 text-black" size={40} />
+              onClick={() => slide(index, "left")}
+              className="bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 left-0 text-black"
+              size={70}
+            />
             <div
-              ref={el => sliderRefs.current[index] = el}
-              className="flex flex-row overflow-x-hidden overflow-y-hidden whitespace-nowrap" 
+              ref={(el) => (sliderRefs.current[index] = el)}
+              className="flex flex-row overflow-x-hidden overflow-y-hidden p object-fill"
             >
               <GamesListItem games={gameSet.games} />
             </div>
             <MdChevronRight
-              onClick={() => slide(index, 'right')}
-              className="bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 right-0 text-black" size={40} />
+              onClick={() => slide(index, "right")}
+              className="bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 left-[1579px] text-black"
+              size={70}
+            />
           </div>
         </div>
       ))}
