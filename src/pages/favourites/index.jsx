df02422b03c649339f94a2777256bd9e968ff000
Brett Roberts <brett.roberts@live.ca>
2023-11-05 00:49:22 -0600

    Couple small changes to ensure user is logged in to fetch favourites

--- src/pages/favourites/index.jsx
+++ src/pages/favourites/index.jsx
@@ -1,7 +1,8 @@
 import { getUserFavoriteGames } from "@/db/queries";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import FavouritesList from "@/components/FavouritesList";
-import SearchBar from "@/components/SearchBar";
+import { AuthContext } from "@/context/AuthContext";
+import Link from "next/link";
 
 const Favourites = ({ games }) => {
   const [gamesFetched, setGamesFetched] = useState(false);
@@ -10,6 +11,7 @@ const Favourites = ({ games }) => {
   const [gptGamesFecthed, setGptGamesFetched] = useState(false);
   const [fullGptGamesFetched, setFullGptGamesFetched] = useState(false);
   const [namesOfFavourites, setNamesOfFavourites] = useState([]);
+  const { user } = useContext(AuthContext);
 
   // Fetch to IGDB API with games from DB:
 
@@ -88,41 +90,60 @@ const Favourites = ({ games }) => {
 
   return (
     <div>
-      {gamesFetched ? (
-        <FavouritesList setOfGames={fetchedGames} />
+      {!user ? (
+        <div className="flex items-center justify-center h-screen ">
+          <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+            <div>
+              <div className="text-xl font-medium text-black">
+                Please login or register to keep track of your favourite games!
+              </div>
+              <Link href={"/"}>
+              <button className="px-4 py-1 border border-transparent text-base font-medium rounded-md text-white bg-violet-500 hover:bg-violet-800 ml-56">
+                Login
+              </button>
+              </Link>
+            </div>
+          </div>
+        </div>
       ) : (
-        <div className="text-violet-500 font-bold text-xl [text-shadow:2px_2px_3px_var(--tw-shadow-color)] shadow-black ">
-          Loading Favourites...
-        </div>
-      )}
-      {!fullGptGamesFetched && gamesFetched && (
-        <div
-          role="status"
-          className="fixed absolute bottom-5 left-[850px] flex justify-center"
-        >
-          <svg
-            aria-hidden="true"
-            class="w-11 h-11 text-gray-200 animate-spin dark:text-gray-600 fill-violet-500"
-            viewBox="0 0 100 101"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-              fill="currentColor"
-            />
-            <path
-              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-              fill="currentFill"
-            />
-          </svg>
-          <p className="absolute bottom-2.5 left-14 text-violet-500 font-bold text-xl [text-shadow:2px_2px_3px_var(--tw-shadow-color)] shadow-black w-[200px]">
-            Our AI is thinking...
-          </p>
-        </div>
-      )}
-      {fullGptGamesFetched && (
-        <FavouritesList setOfGames={[suggestedFavourites]} />
+        <>
+          {gamesFetched ? (
+            <FavouritesList setOfGames={fetchedGames} />
+          ) : (
+            <div className="text-violet-500 font-bold text-xl [text-shadow:2px_2px_3px_var(--tw-shadow-color)] shadow-black ">
+              Loading Favourites...
+            </div>
+          )}
+          {!fullGptGamesFetched && gamesFetched && (
+            <div
+              role="status"
+              className="fixed absolute bottom-5 left-[850px] flex justify-center"
+            >
+              <svg
+                aria-hidden="true"
+                class="w-11 h-11 text-gray-200 animate-spin dark:text-gray-600 fill-violet-500"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+              <p className="absolute bottom-2.5 left-14 text-violet-500 font-bold text-xl [text-shadow:2px_2px_3px_var(--tw-shadow-color)] shadow-black w-[200px]">
+                Our AI is thinking...
+              </p>
+            </div>
+          )}
+          {fullGptGamesFetched && (
+            <FavouritesList setOfGames={[suggestedFavourites]} />
+          )}
+        </>
       )}
     </div>
   );
@@ -133,7 +154,7 @@ export default Favourites;
 export async function getServerSideProps() {
   let games;
   try {
-    games = await getUserFavoriteGames(1);
+    games = await getUserFavoriteGames(user.id);
   } catch (error) {
     console.error("Error fetching initial data: ", error);
     return { props: { games: [] } };
