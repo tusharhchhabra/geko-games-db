194c941dd2dde394b34c695173c975dfce555af3
Brett Roberts <brett.roberts@live.ca>
2023-11-06 11:27:41 -0700

    Add styling to egg to match home page components

--- src/components/EasterEgg.jsx
+++ src/components/EasterEgg.jsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
 
 const EasterEgg = () => {
-  const handleClick = () => {};
-
   return (
-    <div className="lg:w-[493px] lg:h-[335px] absolute bottom-[-80px] left-16 mt-9">
-      <Link href={'/easteregg'}>
-        <img src="/assets/whackaBug.png" alt="Game Cover" />
-      </Link>
+    <div>
+      <h2 className="text-violet-500 font-bold p-2 ml-10 mt-5 text-4xl [text-shadow:2px_2px_3px_var(--tw-shadow-color)] shadow-black ">
+        Easter 🥚
+      </h2>
+
+      <div className="w-[440px] h-[352px] absolute bottom-[-200px] left-[-91px] mt-9 rounded-xl">
+        <Link href={"/easteregg"}>
+          <img src="/assets/whackaBug.png" alt="Game Cover" />
+        </Link>
+      </div>
     </div>
   );
 };
