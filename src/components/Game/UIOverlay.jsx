e3db008e1820940badcd41b93cda3a1b39b3d348
Brett Roberts <brett.roberts@live.ca>
2023-11-09 12:41:39 -0700

    Refactor win/lose logic

--- src/components/Game/UIOverlay.jsx
+++ src/components/Game/UIOverlay.jsx
@@ -4,15 +4,27 @@ import Link from "next/link";
 const UIOverlay = ({ score }) => {
   const [showInitialMessage, setShowInitialMessage] = useState(true);
   const [clock, setClock] = useState(68000);
+  const [hasWon, setHasWon] = useState(false);
 
   useEffect(() => {
-    if (clock > 0) {
-      const timer = setInterval(() => {
+    let timer;
+    if (clock > 0 && !hasWon) {
+      timer = setInterval(() => {
         setClock((prevClock) => prevClock - 1000);
       }, 1000);
-      return () => clearInterval(timer);
+    } else if (hasWon) {
+      clearInterval(timer);
     }
-  }, [clock]);
+
+    return () => clearInterval(timer);
+  }, [clock, hasWon]);
+
+  useEffect(() => {
+    if (score >= 500 && !hasWon) {
+      setHasWon(true);
+      setClock(0);
+    }
+  }, [score, hasWon]);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -21,13 +33,20 @@ const UIOverlay = ({ score }) => {
     return () => clearTimeout(timer);
   }, []);
 
-  const timeOrLoseMessage = () => {
-    if (clock <= 0) {
-      return "You Lose!";
-    }
+  const formatTime = () => {
     const minutes = Math.floor(clock / 60000);
     const seconds = ((clock % 60000) / 1000).toFixed(0);
     return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  };
+
+  const timeOrLoseMessage = () => {
+    if (hasWon) {
+      return "You Win!";
+    } else if (clock <= 0) {
+      return "You Lose!";
+    } else {
+      return formatTime();
+    }
   };
 
   return (
@@ -50,36 +69,32 @@ const UIOverlay = ({ score }) => {
         </div>
       )}
 
-      {score < 500 && clock > 0 ? (
-        <div className="w-max p-4 bg-black bg-opacity-70 text-white rounded-md shadow-md">
-          <div className="text-xl font-bold"> Bugs Whacked: {score}</div>
-          <div className="flex justify-center font-bold text-xl">
-            Time left: <span className="text-red-500">{timeOrLoseMessage()}</span>
-          </div>
-        </div>
-      ) : clock <= 0 ? (
+      {hasWon ? (
         <>
-        <div className="w-max p-4 bg-red-500 bg-opacity-70 text-black rounded-md shadow-md">
-          <div className="text-xl font-bold">{timeOrLoseMessage()} </div>
-        </div>
-        <div className="w-max mt-4 p-4 bg-violet-500 text-black rounded-md shadow-md">
-          <Link href="/">
-            <div className="text-xl font-bold underline">Back to Homepage</div>
-          </Link>
-        </div>
-      </>
-      ) : (
-        <>
-          <div className="w-max mb-4 p-4 bg-black bg-opacity-70 text-white rounded-md shadow-md">
-            <div className="text-xl font-bold"> You Win! </div>
+          <div className="w-max mb-4 p-4 bg-green-500 text-white rounded-md shadow-md">
+            <div className="text-xl font-bold">You Win!</div>
           </div>
           <div className="w-max p-4 text-black rounded-md shadow-md bg-violet-500">
             <Link href="/">
-              <div className="text-xl font-bold underline">
-                Back to Homepage
-              </div>
+              <div className="text-xl font-bold underline">Back to Homepage</div>
             </Link>
           </div>
+        </>
+      ) : (
+        <>
+          <div className="w-max p-4 bg-black bg-opacity-70 text-white rounded-md shadow-md">
+            <div className="text-xl font-bold">Bugs Whacked: {score}</div>
+            <div className="text-xl font-bold">
+              Time left: <span className="text-red-500">{timeOrLoseMessage()}</span>
+            </div>
+          </div>
+          {clock <= 0 && (
+            <div className="w-max mt-4 p-4 bg-violet-500 text-black rounded-md shadow-md">
+              <Link href="/">
+                <div className="text-xl font-bold underline">Back to Homepage</div>
+              </Link>
+            </div>
+          )}
         </>
       )}
     </div>
